c74427fc0eaf0762f9e691c81a17af61edf66f1d
keshavsoft <nknnkumar@yahoo.com>
2026-07-16 21:38:42 +0530

    vertical classes nice

---
+++ bin/vertical/v2/commands/vertical/template/v13/ai.js
@@ -0,0 +1,12 @@
+import KSAiVertical from "./Core/KSAiVertical.js";
+
+window.KSAiVertical = KSAiVertical;
+window.KSAiVertical.defaults = KSAiVertical.defaults;
+
+window.ks = window.ks || {};
+window.ks.classes = window.ks.classes || {};
+window.ks.classes.vertical = KSAiVertical;
+window.ks.classes.verticalVersion = "v13";
+// console.log("aaaaaaaaaaaaa---------------- ");
+
+export default KSAiVertical;
